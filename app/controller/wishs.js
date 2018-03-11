3735421657a6d31ab9b4c70553d5244c4202fdff
linchanglin <493043444@qq.com>
2018-03-11 14:58:20 +0800

    update

--- app/controller/wishs.js
+++ app/controller/wishs.js
@@ -47,13 +47,10 @@ class WishsController extends Controller {
     }
     async create() {
         const ctx = this.ctx;
-        // ctx.validate(createRule);
-        // const req = ctx.request.body;
-        // let res = await ctx.service.wishs.create(req);
-        // ctx.body = { id: res.id };
-        // ctx.status = 201;
-
-        ctx.body = 'post create';
+        ctx.validate(createRule);
+        const req = ctx.request.body;
+        let res = await ctx.service.wishs.create(req);
+        ctx.body = { id: res.id };
         ctx.status = 201;
     }
     async destroy() {
